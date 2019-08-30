485e96f544ceda39ef8a12509496f509d280f14e
山科雄矢 <yuya.e.y@gmail.com>
2019-08-31 05:14:35 +0900

    InstallItemAddFunction

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -144,22 +144,52 @@ function initMap() {
 
 
         });
-        // 地図表示領域をマーカー位置に合わせて拡大
-        bounds.extend(marker.position);
-
+        //Window内のボタンを表示
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                infoWindow.setContent("Kanzumeの名前:" + kanzumes[i]["name"] + " 場所:" + locations[i]["address"]);
+
+                var contentinfo = document.createElement("div");
+                var kanzumeinfo = document.createTextNode(("Kanzumeの名前: " + kanzumes[i]["name"] + " " + "場所: " + locations[i]["address"]).toString());
+                contentinfo.appendChild(kanzumeinfo);
+                var addItem_btn = document.createElement("button");
+                var kanzume_id = kanzumes[i]["id"];
+                addItem_btn.innerText = "このKanzumeにアイテムを入れる";
+                google.maps.event.addDomListener(addItem_btn, "click", function() {
+                    $.ajax({
+                        type: "GET",
+                        url: "/items/new",
+                        data: {
+                            kanzume_id: kanzume_id.toString()
+                        },
+                        dataType: 'html',
+                        success: function(data) {
+                            var win = window.open();
+                            win.document.write(data);
+                        }
+                    });
+                });
+                contentinfo.appendChild(addItem_btn);
+                if (nowmarker) { //現在地マーカーが設置されている場合は消去
+                    nowmarker.setMap(null);
+                }
+                infoWindow.setContent(
+                    contentinfo
+                );
+
                 infoWindow.open(map, marker);
             }
         })(marker, i));
+
+
         // 引数に指定した矩形領域を地図に収める
         map.fitBounds(bounds);
+        // 地図表示領域をマーカー位置に合わせて拡大
+        bounds.extend(marker.position);
 
 
 
-    }
-    // クリックした時情報を取得
+    };
+    // 地図上をクリックした時情報を取得
     var clickedLatlng;
     var nowmarker;
     $(function addKanzume() {
@@ -211,9 +241,11 @@ function initMap() {
                 console.log(geoCodeResults);
             }
 
+
             //Window内のボタン
+
             var a_btn = document.createElement("button");
-            a_btn.innerText = " a";
+            a_btn.innerText = "ここにKanzumeを追加する";
             google.maps.event.addDomListener(a_btn, "click", function() {
                 $.ajax({
                     type: "GET",
@@ -234,11 +266,12 @@ function initMap() {
                 a_btn
             );
             infoWindow.open(map, nowmarker); // 吹き出しの表示
+
         }));
     });
 
 
-
+    //現在地ボタン
     var geoOptions = {
         enableHighAccuracy: true, //精度を高める
         timeout: 6000, //タイムアウトは6秒
@@ -273,7 +306,80 @@ function initMap() {
                     enableHighAccuracy: true
                 });
         }
+    });
 
+    //検索ボタン
+    var searchmarker;
+    document.getElementById('search').addEventListener('click', function() {
+
+        var place = document.getElementById('keyword').value;
+        var geocoder = new google.maps.Geocoder(); // geocoderのコンストラクタ
+
+        geocoder.geocode({
+            address: place
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                for (var i in results) {
+                    if (results[0].geometry) {
+                        // 緯度経度を取得
+                        var s_latlng = results[0].geometry.location;
+                        // 住所を取得
+                        var address = results[0].formatted_address;
+                        // 検索結果地が含まれるように範囲を拡大
+                        map.setZoom(16);
+                        map.setCenter(s_latlng);
+                        // マーカーのセット
+                        setMarker(s_latlng);
+                        // マーカーへの吹き出しの追加
+                        setInfoW(place);
+                        // マーカーにクリックイベントを追加
+                        searchmarkerEvent();
+                    }
+                }
+            } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+                alert("見つかりません");
+            } else {
+                console.log(status);
+                alert("エラー発生");
+            }
+        });
 
     });
+
+    // マーカーのセットを実施する
+    function setMarker(setplace) {
+        // 既にあるマーカーを削除
+        deleteMakers();
+
+        var iconUrl = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+        searchmarker = new google.maps.Marker({
+            position: setplace,
+            map: map,
+            icon: iconUrl
+        });
+    }
+
+    //マーカーを削除する
+    function deleteMakers() {
+        if (searchmarker != null) {
+            searchmarker.setMap(null);
+        }
+        searchmarker = null;
+    }
+
+    // マーカーへの吹き出しの追加
+    function setInfoW(place) {
+        infoWindow = new google.maps.InfoWindow({
+            content: "<br><br>" + place + "<br><br>"
+        });
+    }
+
+    // クリックイベント
+    function searchmarkerEvent() {
+        searchmarker.addListener('click', function() {
+            infoWindow.open(map, searchmarker);
+        });
+    }
+
 }
