ff80ce096cb73c607663a57ab5d2cc962e198991
山科雄矢 <yuya.e.y@gmail.com>
2019-10-03 04:04:40 +0900

    introduceCFtoimages

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,37 +1,34 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    # アクセスキー
-    aws_access_key_id:     'AKIA5BYIIW7VVXG77TFW',
-    # シークレットキー
-    aws_secret_access_key: 'IiZO3X5SKYvwKUrbjpg4Ns6BttfoPfHofvIl2TPS',
-    # Tokyo
-    region:                'ap-northeast-1',
-  }
+   if Rails.env.production?
 
-  # 公開・非公開の切り替え
-  config.fog_public     = true
-  # キャッシュの保存期間
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+      config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+        provider:              'AWS',
+        # アクセスキー
+        aws_access_key_id:     'AKIA5BYIIW7VVXG77TFW',
+        # シークレットキー
+        aws_secret_access_key: 'IiZO3X5SKYvwKUrbjpg4Ns6BttfoPfHofvIl2TPS',
+        # Tokyo
+        region:                'ap-northeast-1',
+      }
 
-  # キャッシュをS3に保存
-  config.cache_storage = :fog
+      # 公開・非公開の切り替え
+      config.fog_public     = true
+      # キャッシュの保存期間
+      config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
 
-  # 環境ごとにS3のバケットを指定
-  case Rails.env
-    when 'production'
+      # キャッシュをS3に保存
+      config.cache_storage = :fog
+
+      # S3のバケットを指定
       config.fog_directory = 'kanmap-pictures'
       config.asset_host = 'https://d1oc5iua8d0deu.cloudfront.net'
 
-    when 'development'
-      config.storage :file # 開発環境:public/uploades下に保存
-      config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+    else
 
-    when 'test'
       config.storage :file # 開発環境:public/uploades下に保存
       config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end
+    end
 end
 
 # 日本語ファイル名の設定
