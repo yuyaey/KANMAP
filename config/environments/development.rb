d9f1e9fe4685be9bd325da9a98875ac7ca686b47
山科雄矢 <yuya.e.y@gmail.com>
2019-10-07 12:40:29 +0900

    change-dockercompose-status

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.web_console.whitelisted_ips = '172.21.0.1'
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
