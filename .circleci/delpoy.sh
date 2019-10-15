9686aed98ef627c54c164cc2a2fa7195742d8916
山科雄矢 <yuya.e.y@gmail.com>
2019-10-16 06:39:27 +0900

    CircleCIandCapistranosetting

---
+++ .circleci/delpoy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ex
+
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
+MYSECURITYGROUP=sg-03e50dae45de6a603
+MYIP=`curl -s ifconfig.me`
+
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
+
+bundle exec cap production deploy
+
+aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
