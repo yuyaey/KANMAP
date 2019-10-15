2c8dce46cba1023138bf78a4e12fa850f8efc519
山科雄矢 <yuya.e.y@gmail.com>
2019-10-16 06:48:08 +0900

    CircleCIandCapistranosetting

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,7 +8,5 @@ MYSECURITYGROUP=sg-03e50dae45de6a603
 MYIP=`curl -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
-
 bundle exec cap production deploy
-
 aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
