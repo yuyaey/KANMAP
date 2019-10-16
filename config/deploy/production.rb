75bff3e8e8937be63c83aecaeec8630a40875b82
山科雄矢 <yuya.e.y@gmail.com>
2019-10-17 05:37:07 +0900

    CircleCIandECRsetting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,8 @@
 server '52.194.138.126', user: 'yuya', roles: %w{app db web} 
 set :ssh_options, keys: '~/.ssh/kanmap_key_rsa'
-config.require_master_key = true
-config.assets.compile = true
+
+# config.require_master_key = true
+# config.assets.compile = true
 
 # server-based syntax
 # ======================
